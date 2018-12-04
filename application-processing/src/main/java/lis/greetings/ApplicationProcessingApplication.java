fd11eb1c1474bba628f931eb3c9b60f3e0f06e43
helga <helga@helga-OEM>
2018-12-04 23:23:02 +0300

    early stage configuration

---
+++ application-processing/src/main/java/lis/greetings/ApplicationProcessingApplication.java
@@ -0,0 +1,12 @@
+package lis.greetings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationProcessingApplication.class, args);
+	}
+}
