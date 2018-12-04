0ff081a2db0f562e8c58c62f3d7f2c94387e759a
helga <helga@helga-OEM>
2018-12-04 23:26:06 +0300

    early stage configuration

--- application-processing/src/main/java/configuration/ApplicationProcessingApplication.java
+++ application-processing/src/main/java/configuration/ApplicationProcessingApplication.java
@@ -1,4 +1,4 @@
-package lis.greetings;
+package configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- application-processing/src/main/java/configuration/ServletInitializer.java
+++ application-processing/src/main/java/configuration/ServletInitializer.java
@@ -1,4 +1,4 @@
-package lis.greetings;
+package configuration;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
